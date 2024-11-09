e1d34cc7855093f55303fa348b83c7580ad0bd87
JeRyWu <1365840492@qq.com>
2024-11-09 16:56:17 +0800

    Update mod.rs

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -63,7 +63,7 @@ use crate::task::{current_task, TaskInfo};
 
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 4]) -> isize {
-    current_task().unwrap().log_syscall(syscall_id);
+    current_task().unwrap().increase_syscall_count(syscall_id);
     match syscall_id {
         SYSCALL_OPEN => sys_open(args[1] as *const u8, args[2] as u32),
         SYSCALL_CLOSE => sys_close(args[0]),
